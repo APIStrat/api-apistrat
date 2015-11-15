2056aff0c0c00fabf5bfa9498944947932ad5589
Kin Lane <info@apievangelist.com>
2015-11-14 22:49:11 -0800

    debug

--- api/methods/m-schedule-day-three.php
+++ api/methods/m-schedule-day-three.php
@@ -8,7 +8,7 @@ $app->get($route, function ()  use ($app){
  	$request = $app->request();
  	$params = $request->params();
 
-  $File = file_get_contents("http://austin2015.apistrat.com/schedule/dayone.json");
+  $File = file_get_contents("http://austin2015.apistrat.com/schedule/daythree.json");
 
   $ReturnObject = $File;
 

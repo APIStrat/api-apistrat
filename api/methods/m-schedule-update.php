7d3168a4074a8863faebfef806b295db889e3974
Kin Lane <info@apievangelist.com>
2015-11-14 21:28:17 -0800

    debug

--- api/methods/m-schedule-update.php
+++ api/methods/m-schedule-update.php
@@ -9,9 +9,9 @@ $app->get($route, function ()  use ($app){
  	$params = $request->params();
 
   $File = file_get_contents("http://austin2015.apistrat.com/schedule/daytwo.json");
-  echo $File;
+  //echo $File;
   $JSONSchedule = json_decode($File);
-
+  var_dump($JSONSchedule);
   //$ReturnObject['updated'] = 1;
   $ReturnObject = $JSONSchedule;
 
